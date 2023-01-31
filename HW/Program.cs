b3b9890f5a147d454a161be48785e2b9b8abe035
Revenko Vladislav <revad8@gmail.com>
2023-01-31 22:27:47 +0300

    реализовал класс и интерфейс

---
+++ HW/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            var a = Console.ReadLine();
+            var b = Console.ReadLine();
+            var c = calc.Addition(a, b);
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+        }
+
+         public interface IAddition
+        {
+             double Addition(string valu1, string value2);
+        }
+
+        public class Calculator : IAddition
+        {
+            public double Addition(string value1, string value2)
+            {
+                var d1 = Double.Parse(value1);
+                var d2 = Double.Parse(value2);
+                return d1 + d2;
+            }
+        }
+
+    }
+}
